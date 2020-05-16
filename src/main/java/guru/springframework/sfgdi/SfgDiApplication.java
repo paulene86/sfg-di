bbe49174bd47e8c3e7b4491a2614e10eee39a102
AF71EH <paulo.cesar.salazaro.guerra@ing.com>
2020-05-17 01:28:03 +0200

    Added EN and ES profiles

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.ConstructorInjectionController;
-import guru.springframework.sfgdi.controllers.MyController;
-import guru.springframework.sfgdi.controllers.PropertyInjectionController;
-import guru.springframework.sfgdi.controllers.SetterInjectionController;
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +10,10 @@ public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		System.out.println("I18nControlleir------------------------");
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayGreeting());
 
 		MyController myController = (MyController) ctx.getBean("myController");
 		String greeting = myController.sayHello();
---
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishService.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("EN")
+@Service("i18nService")
+public class I18nEnglishService implements GreetingService {
+	@Override
+	public String saySomething() {
+		return "Hello world in English";
+	}
+}
